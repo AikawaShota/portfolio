a72bf7f614074caac4a5d96ee19409820df3560e
AikawaShota <git@a-shota.com>
2024-10-27 16:56:46 +0900

    feat: グローバルナビゲーションの追加
    Changes:
    Header.jsxにグローバルナビゲーションを追加
    index.cssにTailwind CSSの設定を追加
    
    Reason:
    ユーザーがサイト内を移動しやすくするため
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: N/A

--- portfolio-vite/src/components/Header.jsx
+++ portfolio-vite/src/components/Header.jsx
@@ -13,6 +13,7 @@ export default function Header() {
             <div>
                 <PortfolioIntro />
             </div>
+            <GlobalNav />
         </header>
     )
 }
@@ -172,4 +173,48 @@ function PortfolioIntro() {
     }, []);
 
     return <div ref={containerRef} />;
+}
+
+function GlobalNav() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">AikawaShota&apos;s Portfolio</a>
+            </div>
+            <div className="navbar-end">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li><a>Profile</a></li>
+                        <li><a>Portfolio</a></li>
+                        <li><a>Contact</a></li>
+                        <li><a>Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Profile</a></li>
+                    <li><a>Portfolio</a></li>
+                    <li><a>Contact</a></li>
+                    <li><a>Blog</a></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
