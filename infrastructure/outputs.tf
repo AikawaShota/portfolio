54baeff4ec99b9dbddad02d52f6a26f8423bee7e
AikawaShota <git@a-shota.com>
2026-03-20 06:12:52 +0900

    infra: cloudfrontのドメイン名を出力するように追記

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,8 @@ output "site_bucket_name" {
   description = "Name of the S3 bucket for the portfolio site."
   value       = aws_s3_bucket.site.bucket
 }
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.site.domain_name
+}
