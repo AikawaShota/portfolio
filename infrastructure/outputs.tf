2e154e1c758a5e02c231a93a79c0158e8285e65f
AikawaShota <git@a-shota.com>
2026-03-21 00:29:55 +0900

    infra: outputにcloudfrontのdistribution_idとgithub_actions_deploy_role_arnを追加

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -12,3 +12,13 @@ output "acm_certificate_domain_validation_options" {
   description = "DNS validation records for the ACM certificate."
   value       = aws_acm_certificate.site.domain_validation_options
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.site.id
+}
+
+output "github_actions_deploy_role_arn" {
+  description = "IAM role ARN for GitHub Actions deploy."
+  value       = aws_iam_role.github_actions_deploy.arn
+}
